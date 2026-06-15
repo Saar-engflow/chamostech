ab92d9e5c4aaa93f8ca23ad67cff7cf5156dc2f4
Saar-engflow <robinsdev2@gmail.com>
2026-06-15 21:17:37 +0200

    fixed missing dependency error

--- components/ui/aspect-ratio.tsx
+++ components/ui/aspect-ratio.tsx
@@ -1,7 +1,23 @@
-"use client"
+"use client";
 
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
+import React from "react";
+import { cn } from "@/lib/utils";
 
-const AspectRatio = AspectRatioPrimitive.Root
+interface AspectRatioProps extends React.HTMLAttributes<HTMLDivElement> {
+  ratio?: number;
+}
+
+const AspectRatio = React.forwardRef<HTMLDivElement, AspectRatioProps>(
+  ({ ratio = 16 / 9, className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn("relative", className)}
+      style={{ paddingBottom: `${(1 / ratio) * 100}%` }}
+    >
+      <div className="absolute inset-0">{props.children}</div>
+    </div>
+  )
+);
+AspectRatio.displayName = "AspectRatio";
 
 export { AspectRatio }
